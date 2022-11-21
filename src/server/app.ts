e84bd92c6fac99f7cd85dbed7fe7cd7b14cb3a67
Alvaro Parada <alvaro.parada92@gmail.com>
2022-11-21 17:26:25 +0100

    Create a whitelist for corsOpions

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import corsOptions from "./corsOptions.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.disable("x-powered-by");
