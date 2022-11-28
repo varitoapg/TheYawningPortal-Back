d90fb2d676a710c46a16f2dc6fb9968b01ba9fcc
Alvaro Parada <alvaro.parada92@gmail.com>
2022-11-28 18:39:40 +0100

    Create getAllCharacters charactersController

---
+++ src/server/controllers/charactersControllers/charactersControllers.ts
@@ -0,0 +1,20 @@
+import type { NextFunction, Response } from "express";
+import Character from "../../../database/models/Character/Character.js";
+import type { CustomRequest } from "../../middleware/auth/types.js";
+
+export const getAllCharacters = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { characters } = req;
+  try {
+    const allCharacters = await Character.find({
+      _id: { $in: characters },
+    });
+
+    res.status(200).json({ allCharacters });
+  } catch (error: unknown) {
+    next(error);
+  }
+};
